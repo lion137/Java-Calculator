e445145171d1155db360724f9b57055470893c62
lion137 <lion137@gmail.com>
2017-06-12 00:18:22 +0100

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,23 @@
+package calcul;
+
+/**
+ * Created by lion on 12/06/17.
+ */
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Calculator<T> {
+    public static void main(String[] args) throws IOException {
+        BinaryTree<String> t1 = new BinaryTree("");
+        parseTree p1= new parseTree();
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            System.out.print("java-simple-calc> ");
+            String input = scan.nextLine();
+            t1 = p1.buildParseTree(input);
+            if (input.equals("quit") || (input.equals("q"))) break;
+            System.out.println(p1.eval(t1));
+        }
+
+    }
+}
